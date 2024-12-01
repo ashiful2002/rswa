9fc188811ff6e1f0e5e83c359db5a6c96b74d8ce
ashiful-islam <ashifulislam2002@gmail.com>
2024-12-01 23:18:00 +0600

    new

--- src/Components/BsCarousel.jsx
+++ src/Components/BsCarousel.jsx
@@ -1,42 +1,11 @@
-// import { useState } from "react";
-// import Carousel from "react-bootstrap/Carousel";
-// import logo from "../assets/logo.png";
-// import ExampleCarouselImage from "../assets/logo.png";
+import React from 'react'
 
-// function BsCarousrl() {
-//   const [index, setIndex] = useState(0);
+const BsCarousel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-//   const handleSelect = (selectedIndex) => {
-//     setIndex(selectedIndex);
-//   };
-
-//   return (
-//     <Carousel activeIndex={index} onSelect={handleSelect}>
-//       <Carousel.Item>
-//         <ExampleCarouselImage />
-//         <Carousel.Caption>
-//           <h3>First slide label</h3>
-//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//         <ExampleCarouselImage />
-//         <Carousel.Caption>
-//           <h3>Second slide label</h3>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//         <ExampleCarouselImage />
-//         <Carousel.Caption>
-//           <h3>Third slide label</h3>
-//           <p>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-//           </p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//     </Carousel>
-//   );
-// }
-
-// export default BsCarousrl;
+export default BsCarousel
